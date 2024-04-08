a564eb3c5dbc88466ff7a7d3bb3073c2990c20f0
Joachimle <joachimelgaard@MacBook-Air-tilhrende-Joachim.local>
2024-04-08 11:15:41 +0200

    Minor changes

--- src/RegionComparator.java
+++ src/RegionComparator.java
@@ -4,5 +4,6 @@ public class RegionComparator implements Comparator<Covid19Data> {
     @Override
     public int compare(Covid19Data d1, Covid19Data d2) {
         return d1.getRegion().compareTo(d2.getRegion());
+
     }
 }
--- src/UserInterface.java
+++ src/UserInterface.java
@@ -2,37 +2,38 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 public class UserInterface {
-    private String command;
-
+    private int command = 99;
+    private final int SENTINEL = 0;
 
     public UserInterface(){
         Scanner sc = new Scanner(System.in);
         FileHandler fh = new FileHandler();
-        ArrayList<Covid19Data> covidData;
-        covidData = fh.loadAllCovid19Data();
+        ArrayList<Covid19Data> covidData = fh.loadAllCovid19Data();
 
         System.out.println("Hvordan ønsker du at sortere din data?");
-        System.out.println("Tast 1 for region");
-        System.out.println("Tast 2 for aldersgruppe");
-        command = sc.nextLine();
-
-        switch (command){
-            case "1" -> {
-                System.out.println("Du har valgt at sortere efter region");
-                Collections.sort(covidData, new RegionComparator());
-                for (Covid19Data data : covidData){
-                    System.out.println(data);
+        while (command != SENTINEL) {
+            System.out.println("Tast 1 for region");
+            System.out.println("Tast 2 for aldersgruppe");
+            System.out.println("Tast 0 for at afslutte");
+            command = sc.nextInt();
+            switch (command) {
+                case 1 -> {
+                    System.out.println("Du har valgt at sortere efter region");
+                    Collections.sort(covidData, new RegionComparator());
+                    for (Covid19Data data : covidData) {
+                        System.out.println(data);
+                    }
                 }
-            }
-            case "2" -> {
-                System.out.println("Du har valgt at sortere efter aldersgruppe");
-                Collections.sort(covidData, new AldersGruppeComparator());
-                for (Covid19Data data : covidData){
-                    System.out.println(data);
+                case 2 -> {
+                    System.out.println("Du har valgt at sortere efter aldersgruppe");
+                    Collections.sort(covidData, new AldersGruppeComparator());
+                    for (Covid19Data data : covidData) {
+                        System.out.println(data);
+                    }
                 }
-            }
-            case "0" -> System.out.println("Du har valgt af afslutte programmet");
+                case 0 -> System.out.println("Du har valgt af afslutte programmet");
 
+            }
         }
     }
 
